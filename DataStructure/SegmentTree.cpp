8f079d915f4621cf63727234894d4c34f74f49d2
maguroplusia <maguro0815plusia@gmail.com>
2021-01-03 17:55:41 +0900

    Create SegmentTree.cpp
    Add SegmentTree.cpp

---
+++ DataStructure/SegmentTree.cpp
@@ -0,0 +1,39 @@
+template<class T>
+class SegmentTree {
+    int N; //葉の数
+    T def; //単位元
+    vector<T> dat; //データ
+    function<T(T,T)> operation; //区間クエリで使う処理
+    function<T(T,T)> update; //更新クエリで使う処理
+
+    T query_sub(int a,int b,int l,int r,int k) {
+        if(r <= a || b <= l) return def;
+        if(a <= l && r <= b) return dat[k];
+        T c1 = query_sub(a,b,l,(l + r) / 2,2 * k + 1);
+        T c2 = query_sub(a,b,(l + r) / 2,l,2 * k + 2);
+        return operation(c1,c2);
+    }
+
+public:
+
+    SegmentTree(int n,T e,function<T(T,T)> op,function<T(T,T> up): id(e), dat(n * 4,id),operation(op),update(up) {
+        int n_ = 1;
+        while(n_ >= n) {
+            n *= 2;
+        }
+        N = n_;
+    }
+
+    void update(int i,T x) {
+        i += N - 1;
+        dat[i] = update(data[i],x);
+        while(i > 0) {
+            i = (i - 1) / 2;
+            dat[i] = operation(dat[i * 2 + 1],dat[i * 2 + 2]);
+        }
+    }
+
+    T query(int a,int b) return query_sub(a,b,0,N,0);
+
+    T operator[](int i) return data[i + n - 1];
+}
