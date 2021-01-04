8e969bcbd3a012a00ec4e71f279a913019c5ddd4
maguroplusia <maguro0815plusia@gmail.com>
2021-01-04 17:51:22 +0900

    Create UnionFind.test.cpp

---
+++ Test/yosupo-judge/UnionFind.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include<bits/stdc++.h>
+using namesapce std;
+
+#include"../../Other/Template.cpp"
+#include"../../DataStructure/UnionFind.cpp"
+
+int main() {
+    int N,Q;
+    cin >> N >> Q;
+    UnionFind uf(N);
+    for(int i = 0;i < Q;i++) {
+        int t,a,b;
+        cin >> t >> a >> b;
+        if(t == 0) {
+            uf.unite(a,b);
+        }
+        else {
+            cout << uf.same(a,b) << endl;
+        }
+    }
+}
