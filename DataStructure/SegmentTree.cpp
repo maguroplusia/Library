af4fe79376926075ca1ac439d006c158bfb6a284
maguroplusia <maguro0815plusia@gmail.com>
2021-01-04 17:10:12 +0900

    Update SegmentTree.cpp

--- DataStructure/SegmentTree.cpp
+++ DataStructure/SegmentTree.cpp
@@ -1,33 +1,34 @@
-template<class T>
+template<typename T>
 class SegmentTree {
     int N; //葉の数
     T def; //単位元
     vector<T> dat; //データ
-    function<T(T,T)> operation; //区間クエリで使う処理
+    function<T(T,T)> operation_; //区間クエリで使う処理
     function<T(T,T)> update_; //更新クエリで使う処理
 
     T query_sub(int a,int b,int l,int r,int k) {
         if(r <= a || b <= l) return def;
         if(a <= l && r <= b) return dat[k];
         T c1 = query_sub(a,b,l,(l + r) / 2,2 * k + 1);
-        T c2 = query_sub(a,b,(l + r) / 2,l,2 * k + 2);
-        return operation(c1,c2);
+        T c2 = query_sub(a,b,(l + r) / 2,r,2 * k + 2);
+        return operation_(c1,c2);
     }
 
 public:
-    SegmentTree(int n,T e,function<T(T,T)> op,function<T(T,T)> up): def(e), dat(n * 4,e),operation(op),update_(up) {
+    SegmentTree(int n,T e,function<T(T,T)> op,function<T(T,T)> up): def(e),operation_(op),update_(up) {
         int n_ = 1;
         while(n > n_) {
             n_ *= 2;
         }
         N = n_;
+        dat = vector<T>(2 * N - 1,def);
     }
 
     //最初にsetする時に呼ぶ
     void set(int i,T x) { dat[i + N - 1] = x;}
     void build() {
         for (int i = N - 2; i >= 0; i--){
-            dat[i] = operation(dat[i * 2 + 1],dat[i * 2 + 2]);
+            dat[i] = operation_(dat[i * 2 + 1],dat[i * 2 + 2]);
         }
     }
 
@@ -37,12 +38,12 @@ public:
         dat[i] = update_(dat[i],x);
         while(i > 0) {
             i = (i - 1) / 2;
-            dat[i] = operation(dat[i * 2 + 1],dat[i * 2 + 2]);
+            dat[i] = operation_(dat[i * 2 + 1],dat[i * 2 + 2]);
         }
     }
 
-    //operator(l,l + 1,…,r - 1)を求める
-    T query(int a,int b) return query_sub(a,b,0,N,0);
+    //operator(a,a + 1,…,b - 1)を求める
+    T query(int a,int b) {return query_sub(a,b,0,N,0);}
 
-    T operator[](int i) return dat[i + N - 1];
+    T operator[](int i) {return dat[i + N - 1];}
 };
