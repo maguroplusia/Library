f8e83e80675c545bdcf09ac66cf7f975dc27f35a
maguroplusia <maguro0815plusia@gmail.com>
2021-01-05 12:22:43 +0900

    Create SegmentTree-RangeMinimumQuery.test.cpp

---
+++ Test/AOJ/SegmentTree-RangeMinimumQuery.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_A"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include"../../Other/Template.cpp"
+#include"../../DataStructure/SegmentTree.cpp"
+
+int main() {
+    int N,Q;
+    cin >> N >> Q;
+    SegmentTree<int> seg(N,numeric_limits<int>::max(),
+    [](int a,int b){return min(a,b);},
+    [](int a,int b){return b;});
+    for(int i = 0;i < Q;i++) {
+        int t;
+        cin >> t;
+        if(t == 0) {
+            int p,x;
+            cin >> p >> x;
+            seg.update(p,x);
+        }
+        else {
+            int l,r;
+            cin >> l >> r;
+            cout << seg.query(l,r) << endl;
+        }
+    }
+}
