fa1ada5de3f30e371e72f3b6fa96d6294e4db347
maguroplusia <maguro0815plusia@gmail.com>
2021-01-05 14:00:59 +0900

    Create WarshallFloyd.test.cpp

---
+++ Test/AOJ/WarshallFloyd.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_C"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include"../../Other/Template.cpp"
+#include"../../Graph/WarshallFloyd.cpp"
+
+int main() {
+    cin >> N;
+    int E;
+    cin >> E;
+    init();
+    for(int i = 0;i < E;i++) {
+        int s,t;
+        ll d;
+        cin >> s >> t >> d;
+        d[s][t] = d;
+    }
+    WarshallFloyd();
+    bool isnegative = false;
+    for(int i = 0;i < N;i++) {
+        if(d[i][i] < 0) isnegative = true;
+    }
+    if(isnegative) {
+        cout << "NEGATIVE CYCLE" << endl;
+        return 0;
+    }
+    for(int i = 0;i < N;i++) {
+        for(int j = 0;j < N;j++) {
+            if(d[i][j] == INF) cout << "INF" << " ";
+            else cout << d[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
