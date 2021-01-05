be54a81a75ee27679ef9854dff03991cabd54ed4
maguroplusia <maguro0815plusia@gmail.com>
2021-01-05 12:22:50 +0900

    Create SegmentTree-PointAddRangeSum.test.cpp

---
+++ Test/yosupo-judge/SegmentTree-PointAddRangeSum.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include"../../Other/Template.cpp"
+#include"../../DataStructure/SegmentTree.cpp"
+
+int main() {
+    int N,Q;
+    cin >> N >> Q;
+    SegmentTree<ll> seg(N,(ll)0,
+    [](ll a,ll b){return a + b;},
+    [](ll a,ll b){return a + b;});
+    for(int i = 0;i < N;i++) {
+        ll A;
+        cin >> A;
+        seg.set(i,A);
+    }
+    seg.build();
+    for(int i = 0;i < Q;i++) {
+        int t;
+        cin >> t;
+        if(t == 0) {
+            int p;
+            ll x;
+            cin >> p >> x;
+            seg.update(p,x);
+        }
+        else {
+            int l,r;
+            cin >> l >> r;
+            cout << seg.query(l,r) << endl;
+        }
+    }
+}
