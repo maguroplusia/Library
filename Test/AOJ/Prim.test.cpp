9a530d1e5d985cbe70ed91a4f36ef1b51a91720e
maguroplusia <maguro0815plusia@gmail.com>
2021-01-18 18:22:48 +0900

    Create Prim.test.cpp

---
+++ Test/AOJ/Prim.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/2/GRL_2_A"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include"../../Other/Template.cpp"
+#include"../../Graph/Prim.cpp"
+
+int main() {
+    cin >> N;
+    int M;
+    cin >> M;
+    for(int i = 0;i < M;i++) {
+        int s,t;
+        ll w;
+        cin >> s >> t >> w;
+        s--;
+        t--;
+        cost[s][t] = w;
+        cost[t][s] = w;
+    }
+    cout << prim() << endl;
+}
