54d7d0f06008ebf32479e61a4bb06445cbc49b1c
maguroplusia <maguro0815plusia@gmail.com>
2021-02-06 17:01:39 +0900

    Update BellmanFord.cpp

--- Graph/BellmanFord.cpp
+++ Graph/BellmanFord.cpp
@@ -1,6 +1,6 @@
 //頂点fromから頂点toへのコストcostの辺
 struct edge{
-	int from,to;
+    int from,to;
     ll cost;
 };
 
@@ -10,35 +10,35 @@ int N,M; //頂点数、辺数
 
 //s番目の頂点から各頂点への最短距離を求める（ただしs番目の頂点から負閉路を辿って行ける場合は-INF）
 void BellmanFord(int s) {
-	for(int i = 0;i < N;i++) {
-		dist[i] = INF;
-	}
-	dist[s] = 0;
-	for(int i = 0;i < N * 2;i++) {
-		for(auto e:es) {
-			if(dist[e.from] < INF && dist[e.from] + e.cost < dist[e.to])  {
-				if(i >= N - 1) {
-					dist[e.to] = -INF;
-				}
-				else {
-					dist[e.to] = dist[e.from] + e.cost;
-				}
-			}
-		}
-	}
+    for(int i = 0;i < N;i++) {
+        dist[i] = INF;
+    }
+    dist[s] = 0;
+    for(int i = 0;i < N * 2;i++) {
+        for(auto e:es) {
+            if(dist[e.from] < INF && dist[e.from] + e.cost < dist[e.to])  {
+                if(i >= N - 1) {
+                    dist[e.to] = -INF;
+                }
+                else {
+                    dist[e.to] = dist[e.from] + e.cost;
+                }
+            }
+        }
+    }
 }
 
 //trueなら負の閉路が存在する
 bool find_negative_roop() {
-	memset(dist,0,sizeof(dist));
-	for(int i = 0;i < N;i++) {
-		for(int j = 0;j < M;j++) {
-			edge e = es[j];
-			if(dist[e.to] > dist[e.from] + e.cost) {
-				dist[e.to] = dist[e.from] + e.cost;
-				if(i == N - 1) return true;
-			}
-		}
-	}
-	return false;
+    memset(dist,0,sizeof(dist));
+    for(int i = 0;i < N;i++) {
+        for(int j = 0;j < M;j++) {
+            edge e = es[j];
+            if(dist[e.to] > dist[e.from] + e.cost) {
+                dist[e.to] = dist[e.from] + e.cost;
+                if(i == N - 1) return true;
+            }
+        }
+    }
+    return false;
 }
