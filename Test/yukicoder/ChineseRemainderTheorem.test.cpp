9140ad3e0a811db3bf9dfd4b5fc508fed15ee38d
maguroplusia <maguro0815plusia@gmail.com>
2021-03-12 21:22:25 +0900

    Update ChineseRemainderTheorem.test.cpp

--- Test/yukicoder/ChineseRemainderTheorem.test.cpp
+++ Test/yukicoder/ChineseRemainderTheorem.test.cpp
@@ -11,5 +11,8 @@ int main() {
     cin >> X1 >> Y1 >> X2 >> Y2 >> X3 >> Y3;
     pair<ll,ll> ret = CRT({X1,X2,X3},{Y1,Y2,Y3});
     if(ret.second == -1) cout << -1 << endl;
-    else cout << ret.first << endl;
+    else {
+        if(ret.first == 0) cout << ret.second << endl;
+        else cout << ret.first << endl;
+    }
 }
