0cf893078f3a7128bba9e1b8301f52cb4635e78e
maguroplusia <maguro0815plusia@gmail.com>
2021-04-20 18:48:21 +0900

    Update BinaryIndexedTree.cpp

--- DataStructure/BinaryIndexedTree.cpp
+++ DataStructure/BinaryIndexedTree.cpp
@@ -3,7 +3,6 @@ class BinaryIndexedTree {
     int N;
     vector<T> bit;
 
-    //a番目までの和を計算する
     T sum_sub(int a) {
         a++;
         T ret = 0;
@@ -17,21 +16,20 @@ class BinaryIndexedTree {
 
 public:
 
-    BinaryIndexedTree(int a): N(a) {
-        bit.assign(++a,0);
+    BinaryIndexedTree(int n): N(n) {
+        bit.assign(++n,0);
     }
 
-    //A[a]にwを加算する
-    void add(int a,T w) {
-        a++;
-        if(a == 0) return;
-        while(a < bit.size()) {
-            bit[a] += w;
-            a += a & -a;
+    void add(int i,T x) {
+        i++;
+        if(i == 0) return;
+        while(i < bit.size()) {
+            bit[i] += x;
+            i += i & -i;
         }
     }
 
     T sum(int a,int b) {
-        return sum_sub(b - 1) - sum_sub(a - 1);
+        return sum_sub(b) - sum_sub(a - 1);
     }
 };
