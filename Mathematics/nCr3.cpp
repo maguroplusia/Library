ab70e8220aad4a13eb1406293eaefc56bc94165f
maguroplusia <maguro0815plusia@gmail.com>
2021-06-05 10:38:37 +0900

    Update nCr3.cpp

--- Mathematics/nCr3.cpp
+++ Mathematics/nCr3.cpp
@@ -10,7 +10,7 @@ void init() {
 }
 
 ll nCr(int n,int r) {
-    if(0 < n || r > n) return 0;
+    if(0 > n || r > n) return 0;
     if(n == 0 || r == 0) return com[n][r] = 1;
     if(com[n][r] != -1) return com[n][r];
     return com[n][r] = nCr(n - 1,r - 1) + nCr(n - 1,r);
