2a0a21bfdbbbf9c26a4de0b2ee5c16a92c6548e6
maguroplusia <maguro0815plusia@gmail.com>
2021-06-14 17:23:21 +0900

    Update Template.cpp

--- Other/Template.cpp
+++ Other/Template.cpp
@@ -1,17 +1,6 @@
 typedef long long ll;
+constexpr int Inf = 2000000030;
+constexpr long long INF= 2000000000000000000;
 
 template<typename T> inline bool chmax(T &a, T b) { if (a < b) { a = b; return true; } return false; }
 template<typename T> inline bool chmin(T &a, T b) { if (a > b) { a = b; return true; } return false; }
-
-template<typename T>
-T modpow(T N, T P, T M){
-    if(P == 0) return 1;
-    if(P < 0) return 0;
-    if(P % 2 == 0){
-        T t = modpow(N, P/2, M);
-        if(M == -1) return t * t;
-        return t * t % M;
-    }
-    if(M == -1) return N * modpow(N,P - 1,M);
-    return N * modpow(N, P-1, M) % M;
-}
