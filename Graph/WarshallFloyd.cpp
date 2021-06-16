a14611be033fcb8aa87353ae0a43dd2f182f6a4e
maguroplusia <maguro0815plusia@gmail.com>
2021-06-16 21:47:42 +0900

    update

--- Graph/WarshallFloyd.cpp
+++ Graph/WarshallFloyd.cpp
@@ -14,7 +14,7 @@ void init() {
     }
 }
 
-void Warshalong longFloyd() {
+void WarshallFloyd() {
     for(int k = 0;k < N;k++) {
         for(int i = 0;i < N;i++) {
             for(int j = 0;j < N;j++) {
