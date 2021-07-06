f13758da502cedf063ee977da7baeed2fb2a4b42
maguroplusia <maguro0815plusia@gmail.com>
2021-07-06 14:36:38 +0900

    update

--- Mathematics/ChineseRemainderTheorem.cpp
+++ Mathematics/ChineseRemainderTheorem.cpp
@@ -10,20 +10,20 @@ long long extgcd(long long a,long long b,long long &x,long long &y) {
     return d;
 }
 
-//CRT(b,m):n次合同方程式を解く。答えは x = retr(mod retm)の形で表される（解が無い場合は(0,-1)の形で表される）
+//CRT(b,m):n次合同方程式を解く。答えは x = R (mod M)の形で表される（解が無い場合は(0,-1)の形で表される）
 pair<long long,long long> CRT(vector<long long> r,vector<long long> m) {
-    if(r.empty() || m.empty()) return make_pair(0,1);
+    if(r.empty() || m.empty()) return {0,1};
     long long R = r.front();
     long long M = m.front();
     for(int i = 1;i < (int)r.size();i++) {
         long long x,y;
         long long d = extgcd(M,m.at(i),x,y);
-        if((r.at(i) - R) % d != 0) return make_pair(0,-1);
+        if((r.at(i) - R) % d != 0) return {0,-1};
         long long tmp = (r.at(i) - R) / d % (m.at(i) / d) * x % (m.at(i) / d);
         R += M * tmp;
         M *= m.at(i) / d;
     }
     R %= M;
     if(R < 0) R += M;
-    return make_pair(R,M);
+    return {R,M};
 }
